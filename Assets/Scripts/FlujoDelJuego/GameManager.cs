492c0bcb0cc7b1b64ecdfe4dee840ba33776dc33
CDelgado0x0 <cdelgado@uji.es>
2025-07-09 14:02:28 +0200

    PrimerGameManagerImplementado

--- Assets/Scripts/FlujoDelJuego/GameManager.cs
+++ Assets/Scripts/FlujoDelJuego/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -13,16 +16,63 @@ public class GameManager : MonoBehaviour
     }
 
     void Start(){
-
+        updateGameState(GameState.menuPrincipal);
     }
 
-    
+    private void OnValidate() //Esto sirve para poder cambiar el estado desde el inspector, comentar si no es necesario.
+    {
+        updateGameState(state);
+    }
 
     public void updateGameState(GameState newState){
-        State = newState;
+        state = newState;
 
         switch(newState){
-            //Insert all cases
+            case GameState.menuPrincipal:
+                // Aquí puedes agregar lógica específica para el menú principal
+                break;
+            case GameState.encenderLaringoscopio:
+                // Lógica para encender el laringoscopio
+                break;
+            case GameState.inflarBalonTuboOrotraqueal:
+                // Lógica para inflar el balón del tubo orotraqueal
+                break;
+            case GameState.posicionarCabezaPaciente:
+                // Lógica para posicionar la cabeza del paciente
+                break;
+            case GameState.introducirCanulaGirada:
+                // Lógica para introducir la cánula girada
+                break;
+            case GameState.girarCanula:
+                // Lógica para girar la cánula
+                break;
+            case GameState.oxigenarPaciente:
+                // Lógica para oxigenar al paciente
+                break;
+            case GameState.introducirLaringoscopio:
+                // Lógica para introducir el laringoscopio
+                break;
+            case GameState.introducirTuboOrotraqueal:
+                // Lógica para introducir el tubo orotraqueal
+                break;
+            case GameState.sacarLaringoscopio:
+                // Lógica para sacar el laringoscopio
+                break;
+            case GameState.inflarBalon:
+                // Lógica para inflar el balón
+                break;
+            case GameState.asegurarTuboEnBoca:
+                // Lógica para asegurar el tubo en la boca
+                break;
+            case GameState.insuflar:
+                // Lógica para insuflar aire
+                break;
+            case GameState.comprobarTuboIntroducidoCorrectamente:
+                // Lógica para comprobar si el tubo está introducido correctamente
+                break;
+            case GameState.simulacionTerminada:
+                // Lógica para finalizar la simulación
+                break;
         }
 
         onGameStateChanged?.Invoke(newState);
