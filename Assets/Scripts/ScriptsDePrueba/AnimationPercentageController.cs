827a5263dda3bd9d3dca67746e4ca17a92ce3593
CDelgado0x0 <cdelgado@uji.es>
2025-07-14 14:03:02 +0200

    AnimationPorSlider

---
+++ Assets/Scripts/ScriptsDePrueba/AnimationPercentageController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AnimationPercentageController : MonoBehaviour
+{
+    public Animator animator;
+    [Range(0f, 1f)]
+    public float animationProgress = 0f;
+    public string animationStateName = "Andando"; // Nombre del estado de la animación en el Animator
+
+    private void Update()
+    {
+        if (animator)
+        {
+            animator.Play(animationStateName, 0, animationProgress);
+            animator.speed = 0; // Detener el avance automático
+        }
+    }
+
+    public void OnPercentageChanged(float value)
+    {
+        animationProgress = value;
+    }
+}
