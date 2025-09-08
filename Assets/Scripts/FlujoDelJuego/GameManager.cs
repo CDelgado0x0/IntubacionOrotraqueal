6dcf645605bb87a7cb5fa2961c7c0d2d6ca46acf
CDelgado0x0 <cdelgado@uji.es>
2025-09-08 10:25:32 +0200

    PasoQuitarMascarilla

--- Assets/Scripts/FlujoDelJuego/GameManager.cs
+++ Assets/Scripts/FlujoDelJuego/GameManager.cs
@@ -115,6 +115,10 @@ public class GameManager : MonoBehaviour
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[16]);
                 LEDPantallas.SetTexture("_EmissionMap", Instrucciones[16]);
                 break;
+                case GameState.desacoplarMascarilla:
+                LEDPantallas.SetTexture("_BaseMap", Instrucciones[16]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[16]);
+                break;
                 case GameState.asegurarTuboEnBoca:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[17]);
                 LEDPantallas.SetTexture("_EmissionMap", Instrucciones[17]);
@@ -169,6 +173,7 @@ public enum GameState{
     conectarJeringa,
     inflarBalon,
     quitarJeringa,
+    desacoplarMascarilla,
     asegurarTuboEnBoca,
     AcoplarCapnogragoYAmbu,
     insuflar,
---
+++ Assets/Scripts/Phases/Phase4/AmbuMask/MaskBehaviour.cs
@@ -0,0 +1,58 @@
+using Oculus.Interaction;
+using UnityEngine;
+
+public class MaskBehaviour : MonoBehaviour
+{
+    private bool scriptActive = false;
+    private Rigidbody myRb;
+
+    [SerializeField] private Grabbable myGrab;
+    [SerializeField] private GrabFreeTransformer FreeGrab;
+
+    public BoxCollider myCollider;
+    public GameObject parentCollider;
+
+    private void DesacoploMascarilla(GameState state)
+    {
+        if (state == GameState.desacoplarMascarilla)
+        {
+            scriptActive = true;
+        }
+        else
+        {
+            scriptActive = false;
+        }
+    }
+
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+        FreeGrab?.Initialize(myGrab);
+        GameManager.onGameStateChanged += DesacoploMascarilla;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.onGameStateChanged -= DesacoploMascarilla;
+    }
+
+    public void disconnectMask()
+    {
+        if (scriptActive)
+        {
+            transform.parent = null;
+            myGrab.InjectOptionalOneGrabTransformer(FreeGrab);
+        }
+    }
+
+    public void onMaskRelease()
+    {
+        if (scriptActive)
+        {
+            parentCollider.SetActive(false);
+            myCollider.isTrigger = false;
+            myRb.isKinematic = false;
+            GameManager.applicationController.updateGameState(GameState.asegurarTuboEnBoca);
+        }
+    }
+}
