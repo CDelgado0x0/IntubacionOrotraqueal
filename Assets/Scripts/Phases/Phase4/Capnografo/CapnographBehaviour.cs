e4c432fdc5dfb532f4c863a691a160e7fd947f98
CDelgado0x0 <cdelgado@uji.es>
2025-09-23 11:42:07 +0200

    ImplementacionCapnografo

---
+++ Assets/Scripts/Phases/Phase4/Capnografo/CapnographBehaviour.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CapnographBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject CapnographHands;
+
+    private Rigidbody myRb;
+
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("OrotraquealDetectable"))
+        {
+            CapnographHands.SetActive(false);
+            myRb.isKinematic = true;
+            transform.position = other.transform.position;
+            transform.rotation = other.transform.rotation;
+        }
+    }
+}
