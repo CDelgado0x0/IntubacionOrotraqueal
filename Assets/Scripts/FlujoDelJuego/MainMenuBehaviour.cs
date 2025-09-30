4b41f11526e1aba6eb9790e3f378699210b5bad5
CDelgado0x0 <cdelgado@uji.es>
2025-09-30 10:30:07 +0200

    MainMenuuuu

--- Assets/Scripts/FlujoDelJuego/MainMenuBehaviour.cs
+++ Assets/Scripts/FlujoDelJuego/MainMenuBehaviour.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
+    [SerializeField] private GameObject spanishMainButtons;
+    [SerializeField] private GameObject englishMainButtons;
+    [SerializeField] private GameObject mainMenu;
+
     public void startButton()
     {
+        mainMenu.SetActive(false);
         GameManager.applicationController.updateGameState(GameState.encenderLaringoscopio);
+    }
+
+    public void changeToSpanish()
+    {
+        spanishMainButtons.SetActive(true);
+        englishMainButtons.SetActive(false);
+        GameManager.applicationController.CambiarIdioma(Language.Spanish);
+    }
+
+    public void changeToEnglish()
+    {
+        spanishMainButtons.SetActive(false);
+        englishMainButtons.SetActive(true);
+        GameManager.applicationController.CambiarIdioma(Language.English);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
     }
 }
